289c537e8764b28091c5d9dfc211c4ece2f4e9af
Patrick Ayres <briceayres@gmail.com>
2016-02-10 11:40:08 -0500

    added properties to parent

---
+++ db/migrate/20160210163432_add_properties_to_parent.rb
@@ -0,0 +1,9 @@
+class AddPropertiesToParent < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :email, :string
+    add_column :users, :street, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210163003) do
+ActiveRecord::Schema.define(version: 20160210163432) do
 
   create_table "users", force: :cascade do |t|
     t.string   "firstName"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20160210163003) do
     t.string   "phoneNumber"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "email"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
   end
 
 end
